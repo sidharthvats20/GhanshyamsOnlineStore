ec80d9941d1d919e7aee26e385c989ad22ccafdf
Sidharth Vats <sidharthvts9@gmail.com>
2022-08-14 16:40:27 +0530

    Landing Page

--- client/src/Features/Navbar/Navbar.tsx
+++ client/src/Features/Navbar/Navbar.tsx
@@ -3,7 +3,8 @@ import {faBirthdayCake, faCake} from  '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar(){
     return(
-        <nav className="navbar navbar-dark bg-dark fixed-top">
+      <div>
+         <nav className="navbar navbar-dark bg-dark fixed-top mb-2">
   <div className="container-fluid col-12">
         <a className="navbar-brand" href="#"><FontAwesomeIcon icon={faCake}/></a>
         <a className="navbar-brand" href="#">Ghanshyam's</a>
@@ -11,15 +12,15 @@ export default function Navbar(){
         <span className="navbar-toggler-icon"></span>
         </button>
     
-    <div className="offcanvas offcanvas-end text-bg-dark" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+    <div className="offcanvas offcanvas-end text-bg-dark" tabIndex={-1} id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Hi, Sidharth</h5>
+        <h5 className="offcanvas-title gold-text" id="offcanvasDarkNavbarLabel">Hi, Sidharth</h5>
         <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div className="offcanvas-body">
         <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link" aria-current="page" href="#">Home</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">About</a>
@@ -29,9 +30,9 @@ export default function Navbar(){
               Categories
             </a>
             <ul className="dropdown-menu dropdown-menu-dark">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
+              <li><a className="dropdown-item" href="#">Cakes</a></li>
+              <li><a className="dropdown-item" href="#">Mocktails</a></li>
+              <li><a className="dropdown-item" href="#">Decor Materials</a></li>
             </ul>
           </li>
         </ul>
@@ -39,5 +40,7 @@ export default function Navbar(){
     </div>
   </div>
 </nav>
+      </div>
+       
     )
 }
