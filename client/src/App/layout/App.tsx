f9b61fd29f1d948e16123a514029a40a48049d20
Sidharth Vats <sidharthvts9@gmail.com>
2022-08-14 23:40:40 +0530

    Added catalog component

--- client/src/App/layout/App.tsx
+++ client/src/App/layout/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Landing from "../../Features/landing/Landing";
-import Catalog from "../../Features/landing/Landing";
-import Navbar from "../../Features/Navbar/Navbar";
+import Catalog from "../../Features/catalog/Catalog";
+import Navbar from "../../Features/navbar/Navbar";
 import { Product } from "../models/Product";
 
 function App() {
@@ -17,17 +17,7 @@ useEffect(() => {
     <div className="app">
       <Navbar/>
       <Landing/>
-      <svg className="wave1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a2d9ff" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-      <div className="productsbg">
-        <h1 className="mt-0">Products</h1>
-        <ul>
-          {products.map((item, index) => (
-          <li key={index}>{item.id} - {item.name} - {item.price}</li>
-      ))}
-      </ul>
-      </div>
-
-
+      <Catalog products={products}/>
     </div>
   );
 }
